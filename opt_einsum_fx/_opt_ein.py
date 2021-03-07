87e88902e4aec56cfb2651409ec46e8e43b000fe
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-03-07 12:07:38 -0700

    Added no shapes fallback

--- opt_einsum_fx/_opt_ein.py
+++ opt_einsum_fx/_opt_ein.py
@@ -15,40 +15,55 @@ def optimize_einsums(graph: fx.Graph) -> fx.Graph:
     # env keeps track of new injected nodes in addition to existing ones,
     # making sure they get into new_graph
     env = {}
+    node_processed: bool = False
     for node in graph.nodes:
+        node_processed = False
         if node.op == 'call_function' and node.target == torch.einsum:
-            # Determine the optimal contraction
-            path, path_info = opt_einsum.contract_path(
-                node.args[0],  # the einstr
-                *[a.shape for a in node.args[1:]],
-                shapes=True
-            )
-            # By wrapping the arguments with proxies,
-            # we can dispatch to opt_einsum and implicitly
-            # add it to the Graph by symbolically tracing it.
-            proxy_args = [
-                fx.Proxy(env[x.name]) if isinstance(x, fx.Node) else x 
-                for x in node.args
-            ]
-            print("path", path)
-            # Use _core_contract to avoid `len()` calls that 
-            # fx can't deal with
-            output_proxy = _core_contract(
-                proxy_args[1:],
-                path_info.contraction_list,
-                backend='torch',
-                evaluate_constants=False
-            )
+            # Get shapes:
+            try:
+                shapes = [a.shape for a in node.args[1:]]
+            except AttributeError:
+                warnings.warn(
+                    f"einsum {repr(node)} lacked shape information; "
+                    "not optimizing. "
+                    "Did you forget to run ShapeProp on this graph?",
+                    RuntimeWarning
+                )
+            else:
+                # We have shapes, so:
+                # Determine the optimal contraction
+                path, path_info = opt_einsum.contract_path(
+                    node.args[0],  # the einstr
+                    *shapes,
+                    shapes=True
+                )
+                # By wrapping the arguments with proxies,
+                # we can dispatch to opt_einsum and implicitly
+                # add it to the Graph by symbolically tracing it.
+                proxy_args = [
+                    fx.Proxy(env[x.name]) if isinstance(x, fx.Node) else x 
+                    for x in node.args
+                ]
+                print("path", path)
+                # Use _core_contract to avoid `len()` calls that 
+                # fx can't deal with
+                output_proxy = _core_contract(
+                    proxy_args[1:],
+                    path_info.contraction_list,
+                    backend='torch',
+                    evaluate_constants=False
+                )
 
-            # Operations on `Proxy` always yield new `Proxy`s, and the
-            # return value of our decomposition rule is no exception.
-            # We need to extract the underlying `Node` from the `Proxy`
-            # to use it in subsequent iterations of this transform.
-            new_node = output_proxy.node
-            env[node.name] = new_node
-        else:
-            # Default case: we don't have a decomposition rule for this
-            # node, so just copy the node over into the new graph.
+                # Operations on `Proxy` always yield new `Proxy`s, and the
+                # return value of our decomposition rule is no exception.
+                # We need to extract the underlying `Node` from the `Proxy`
+                # to use it in subsequent iterations of this transform.
+                new_node = output_proxy.node
+                env[node.name] = new_node
+                node_processed = True
+
+        if not node_processed:
+            # Default case: just copy the node over into the new graph.
             new_node = new_graph.node_copy(node, lambda x: env[x.name])
             env[node.name] = new_node
 
--- tests/test_einsum_optimizer.py
+++ tests/test_einsum_optimizer.py
@@ -1,3 +1,6 @@
+import warnings
+import pytest
+
 import torch
 import torch.fx
 from torch.fx.passes.shape_prop import ShapeProp
@@ -5,16 +8,16 @@ from torch.fx.passes.shape_prop import ShapeProp
 from opt_einsum_fx import optimize_einsums
 
 
-def test_optimize_einsums():
-    def func(x, y):
-        return torch.einsum("ij,jk->ik", x, y)
+def einmatmul(x, y):
+    return torch.einsum("ij,jk->ik", x, y)
 
+def test_optimize_einsums():
     x = torch.randn(3, 4)
     y = torch.randn(4, 5)
 
-    func_res = func(x, y)
+    func_res = einmatmul(x, y)
 
-    func_fx = torch.fx.symbolic_trace(func)
+    func_fx = torch.fx.symbolic_trace(einmatmul)
     sp = ShapeProp(func_fx)
     sp.run(x, y)
 
@@ -27,3 +30,17 @@ def test_optimize_einsums():
 
     func_opt_res = func_fx(x, y)
     assert torch.allclose(func_opt_res, func_fx_res)
+
+
+def test_fallback():
+    # If there is no shape propagation, it should warn
+    # and not do anything.
+    func_fx = torch.fx.symbolic_trace(einmatmul)
+    old_code = func_fx.code
+
+    with pytest.warns(RuntimeWarning):
+        graph_opt = optimize_einsums(func_fx.graph)
+
+    func_fx.graph = graph_opt
+    func_fx.recompile()
+    assert old_code == func_fx.code
