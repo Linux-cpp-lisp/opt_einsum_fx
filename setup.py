f32722a01eb3c2c0e4d5054ed9854ee98e828f8d
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-03-08 15:06:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='opt_einsum_fx',
-    version='0.0.1',
-    packages=['opt_einsum_fx'],
+    name="opt_einsum_fx",
+    description="Einsum optimization using opt_einsum and PyTorch FX graph rewriting",
+    version="0.0.1",
+    url="https://github.com/Linux-cpp-lisp/opt_einsum_fx",
+    packages=["opt_einsum_fx"],
 )
