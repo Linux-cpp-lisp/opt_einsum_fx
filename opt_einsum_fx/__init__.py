d78ee8f94416493578d4d1e7374994f729231e35
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-05-26 15:17:58 -0600

    use __all__

--- opt_einsum_fx/__init__.py
+++ opt_einsum_fx/__init__.py
@@ -1,3 +1,11 @@
-from ._script import jitable  # noqa
-from ._opt_ein import optimize_einsums, optimize_einsums_full  # noqa
-from ._fuse import fuse_einsums, fuse_scalars  # noqa
+from ._script import jitable
+from ._opt_ein import optimize_einsums, optimize_einsums_full
+from ._fuse import fuse_einsums, fuse_scalars
+
+__all__ = [
+    "jitable",
+    "optimize_einsums",
+    "optimize_einsums_full",
+    "fuse_einsums",
+    "fuse_scalars",
+]
