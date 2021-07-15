44590ea88c04617d8f4f87cf1ffaf38773047de1
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-07-15 16:12:56 -0400

    initial tests

---
+++ tests/test_cuTENSOR.py
@@ -0,0 +1,54 @@
+import pytest
+
+import random
+
+import torch
+from opt_einsum.parser import find_output_str
+
+import opt_einsum_fx  # noqa: F401
+
+
+if not torch.cuda.is_available():
+    pytest.skip("No CUDA; skipping cuTENSOR tests.", allow_module_level=True)
+
+
+@pytest.mark.parametrize("einstr", ["ij,jk->ik"])
+def test_like_torch(einstr):
+    device = "cuda"
+    dtype = torch.float
+    # Shapes
+    modes = set(einstr) - {",", "-", ">"}
+    extents = {m: random.choice([1, 2, 3, 5, 11, 27, 50]) for m in modes}
+    if "->" in einstr:
+        modesAB, modesC = einstr.split("->")
+    else:
+        modesAB = einstr, find_output_str(einstr)
+    modesA, modesB = modesAB.split(",")
+    # Make tensors
+    A = torch.randn(
+        size=[extents[m] for m in modesA],
+        dtype=dtype,
+        device=device,
+    )
+    B = torch.randn(
+        size=[extents[m] for m in modesB],
+        dtype=dtype,
+        device=device,
+    )
+    # Get the truth output
+    A_true, B_true = A.clone(), B.clone()
+    A_true.requires_grad_(True)
+    B_true.requires_grad_(True)
+    true_out = torch.einsum(einstr, A_true, B_true)
+    true_grad_A = torch.autograd.grad(true_out.sum(), A_true, retain_graph=True)[0]
+    true_grad_B = torch.autograd.grad(true_out.sum(), B_true)[0]
+    # Get the cuTENSOR outputs
+    A.requires_grad_(True)
+    B.requires_grad_(True)
+    out = torch.ops._opt_einsum_fx.einsum(einstr, A, B)
+    grad_A = torch.autograd.grad(out.sum(), A, retain_graph=True)[0]
+    grad_B = torch.autograd.grad(out.sum(), B)[0]
+    # Check
+    assert torch.allclose(true_out, out)
+    assert torch.allclose(true_grad_A, grad_A)
+    assert torch.allclose(true_grad_B, grad_B)
