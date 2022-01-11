b0b542f18ecacb4400064b92d5ec2886280631b0
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2022-01-11 00:01:16 -0700

    test nondynamic graph

--- tests/test_tracing.py
+++ tests/test_tracing.py
@@ -1,6 +1,7 @@
 import pytest
 
 import torch
+from functorch import make_fx
 
 from opt_einsum_fx.tracing import make_fx_dynamic
 
@@ -35,6 +36,27 @@ def test_trace_like_func(func):
     truth_outs = [func(*e) for e in test_inputs]
     # running this checks that it can run with dynamic shapes
     traced_outs = [traced(*e) for e in test_inputs]
+    for truth_out, traced_out in zip(truth_outs, traced_outs):
+        for truth_tensor, traced_tensor in zip(truth_out, traced_out):
+            assert truth_tensor.shape == traced_tensor.shape
+            assert torch.allclose(truth_tensor, traced_tensor)
+
+
+def test_no_dynamic_simple():
+    func = simple
+    bdims_trace = [1, 3, 4]
+    bdims_test = [2, 7, 5]
+    trace_inputs = [(torch.randn(bdim, 5),) for bdim in bdims_trace]
+    test_inputs = [(torch.randn(bdim, 5),) for bdim in bdims_test]
+    # now trace out the function
+    traced = make_fx(func)(*trace_inputs[0])
+    traced_dynamic = make_fx_dynamic(func, example_inputs=trace_inputs)
+    # they should be the same, but with one dynamic shape extraction
+    # which is one getattr and one getitem
+    assert len(traced.graph.nodes) + 2 == len(traced_dynamic.graph.nodes)
+    # and should give same results
+    truth_outs = [traced(*e) for e in test_inputs]
+    traced_outs = [traced_dynamic(*e) for e in test_inputs]
     for truth_out, traced_out in zip(truth_outs, traced_outs):
         for truth_tensor, traced_tensor in zip(truth_out, traced_out):
             assert truth_tensor.shape == traced_tensor.shape
